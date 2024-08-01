71f7bbc396a0dfcb4bd1cfd21d57fb20596752cf
martinezmilton <martinez.rodriguez.miltonangel@usfx.bo>
2024-08-01 19:21:27 -0400

    Pokedex-with-React-Native-task

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import { HomeScreen } from './src/HomeScreen';
+
+export default function App() {
+  return (
+      <HomeScreen />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/pokemon.ts
@@ -0,0 +1,7 @@
+//Estructura de los datos pokemon
+export interface Pokemon {
+    id: number;
+    name: string;
+    types: string[];
+    avatar: string;
+}
